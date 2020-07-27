0ab7a974cff1531b7b462586190a9373d8885f4d
chanhenry54 <hechan@ucsd.edu>
2020-07-26 22:36:57 -0700

    started work on createpokemon page, made basic nav

---
+++ client/src/components/Navbar/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+
+import './style.css';
+
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <div className="title">
+                <h1>Pokemon Generator</h1>
+            </div>
+            <div className="nav-buttons">
+                {/* wrap a button within a link, have one for create and view pokemon */}
+                <p className="button">Create New Pokemon</p>
+                <p className="button">View All Pokemon</p>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ client/src/pages/CreatePokemon.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+import Navbar from '../components/Navbar';
+
+const CreatePokemon = () => {
+    return (
+        <div>
+            <Navbar />
+            <p>Hello</p>
+        </div>
+    );
+};
+
+export default CreatePokemon;
