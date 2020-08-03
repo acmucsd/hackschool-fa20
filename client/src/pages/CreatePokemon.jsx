8830d0a028423689a93e6246aae4ac16300b1bfd
chanhenry54 <hechan@ucsd.edu>
2020-08-02 19:12:15 -0700

    tweaked some style and input handling

--- client/src/pages/CreatePokemon.jsx
+++ client/src/pages/CreatePokemon.jsx
@@ -5,14 +5,15 @@ import Navbar from '../components/Navbar';
 import CanvasDraw from 'react-canvas-draw';
 
 const CreatePokemon = () => {
-    const [name, setName] = useState();
+    const [name, setName] = useState('');
     const [color, setColor] = useState('#000');
 
     /**
      * Method to handle the creation of a Pokemon on form submit.
      */
-    const handleCreatePokemon = () => {
-        return;
+    const handleCreatePokemon = (e) => {
+        e.preventDefault();
+        // e.target.name.value
     };
 
     /**
@@ -20,14 +21,14 @@ const CreatePokemon = () => {
      */
     const randName = () => {
         // use some API to generate random pokemon name
-        setName("Henry");
+        setName(Math.random());
     };
 
     return (
         <div>
             <Navbar />
             <div className="create-page">
-                <div class="drawing">
+                <div className="drawing">
                     <h2>Draw your Pokemon here!</h2>
                     <div className="canvas">
                         <CanvasDraw hideGrid={true}
@@ -37,24 +38,24 @@ const CreatePokemon = () => {
                             style={{ width: '100%', height: '100%' }} />
                     </div>
                     <div className="color-picker">
-                        <label for="select-color">Color</label>
+                        <label htmlFor="select-color">Color</label>
                         <input type="color" id="select-color" value={color} onChange={(e) => setColor(e.target.value)}></input>
                     </div>
                 </div>
                 <div className="pokemon-form">
                     <h2>Create a Pokemon</h2>
                     <form onSubmit={handleCreatePokemon}>
-                        <div class="name-row">
-                            <label for="name">Name</label>
-                            <input type="text" id="name" name="name" value={name}></input>
+                        <div className="name-row">
+                            <label htmlFor="name">Name</label>
+                            <input type="text" id="name" name="name" defaultValue={name} required></input>
                             <button type="button" onClick={randName}>Generate Random Name</button>
                         </div>
-                        <div class="desc-row">
-                            <label for="desc">Description</label>
-                            <input type="text" id="desc" name="desc"></input>
+                        <div className="desc-row">
+                            <label htmlFor="desc">Description</label>
+                            <input type="text" id="desc" name="desc" required></input>
                         </div>
-                        <div class="type-row">
-                            <label for="type1">Type 1</label>
+                        <div className="type-row">
+                            <label htmlFor="type1">Type 1</label>
                             <select id="type1" name="type1">
                                 <option>Normal</option>
                                 <option>Fire</option>
@@ -75,7 +76,7 @@ const CreatePokemon = () => {
                                 <option>Ghost</option>
                                 <option>Steel</option>
                             </select>
-                            <label for="type2">Type 2</label>
+                            <label htmlFor="type2">Type 2</label>
                             <select id="type2" name="type2">
                                 <option value=""></option>
                                 <option>Normal</option>
@@ -98,10 +99,10 @@ const CreatePokemon = () => {
                                 <option>Steel</option>
                             </select>
                         </div>
-                        <div class="move-row">
-                            <label for="move1">Move 1</label>
-                            <input type="text" id="move1" name="move1"></input>
-                            <label for="move1-type">Type</label>
+                        <div className="move-row">
+                            <label htmlFor="move1">Move 1</label>
+                            <input type="text" id="move1" name="move1" required></input>
+                            <label htmlFor="move1-type">Type</label>
                             <select id="move1-type" name="move1-type">
                                 <option>Normal</option>
                                 <option>Fire</option>
@@ -122,13 +123,13 @@ const CreatePokemon = () => {
                                 <option>Ghost</option>
                                 <option>Steel</option>
                             </select>
-                            <label for="move1-power">Power</label>
+                            <label htmlFor="move1-power">Power</label>
                             <input type="number" id="move1-power" name="move1-power" min="0" max="100" defaultValue="0"></input>
                         </div>
-                        <div class="move-row">
-                            <label for="move2">Move 2</label>
-                            <input type="text" id="move2" name="move2"></input>
-                            <label for="move2-type">Type</label>
+                        <div className="move-row">
+                            <label htmlFor="move2">Move 2</label>
+                            <input type="text" id="move2" name="move2" required></input>
+                            <label htmlFor="move2-type">Type</label>
                             <select id="move2-type" name="move2-type">
                                 <option>Normal</option>
                                 <option>Fire</option>
@@ -149,13 +150,13 @@ const CreatePokemon = () => {
                                 <option>Ghost</option>
                                 <option>Steel</option>
                             </select>
-                            <label for="move2-power">Power</label>
+                            <label htmlFor="move2-power">Power</label>
                             <input type="number" id="move2-power" name="move2-power" min="0" max="100" defaultValue="0"></input>
                         </div>
-                        <div class="move-row">
-                            <label for="move3">Move 3</label>
-                            <input type="text" id="move3" name="move3"></input>
-                            <label for="move3-type">Type</label>
+                        <div className="move-row">
+                            <label htmlFor="move3">Move 3</label>
+                            <input type="text" id="move3" name="move3" required></input>
+                            <label htmlFor="move3-type">Type</label>
                             <select id="move3-type" name="move3-type">
                                 <option>Normal</option>
                                 <option>Fire</option>
@@ -176,13 +177,13 @@ const CreatePokemon = () => {
                                 <option>Ghost</option>
                                 <option>Steel</option>
                             </select>
-                            <label for="move3-power">Power</label>
+                            <label htmlFor="move3-power">Power</label>
                             <input type="number" id="move3-power" name="move3-power" min="0" max="100" defaultValue="0"></input>
                         </div>
-                        <div class="move-row">
-                            <label for="move4">Move 4</label>
-                            <input type="text" id="move4" name="move4"></input>
-                            <label for="move4-type">Type</label>
+                        <div className="move-row">
+                            <label htmlFor="move4">Move 4</label>
+                            <input type="text" id="move4" name="move4" required></input>
+                            <label htmlFor="move4-type">Type</label>
                             <select id="move4-type" name="move4-type">
                                 <option>Normal</option>
                                 <option>Fire</option>
@@ -203,7 +204,7 @@ const CreatePokemon = () => {
                                 <option>Ghost</option>
                                 <option>Steel</option>
                             </select>
-                            <label for="move4-power">Power</label>
+                            <label htmlFor="move4-power">Power</label>
                             <input type="number" id="move4-power" name="move4-power" min="0" max="100" defaultValue="0"></input>
                         </div>
                         <input type="submit" value="Submit"></input>
