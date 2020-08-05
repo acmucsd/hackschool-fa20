962451379ad47f15777d7e73e1706855cdee3810
Garrett Luu <garrettluu131@gmail.com>
2020-08-04 17:18:20 -0700

    Updated pokemon database service

--- server/services/pokemon.js
+++ server/services/pokemon.js
@@ -2,21 +2,35 @@ const Pokemon = require('../models/pokemon');
 const config = require('../config');
 const axios = require('axios');
 
+/**
+ * Returns all pokemon in the database
+ */
 async function findAll() {
   return await Pokemon.find().exec();
 }
 
+/**
+ * Queries the database for a specific pokemon based on the given id
+ * @param {id} id 
+ */
 async function findById(id) {
-  // TODO: Database call
+  return await Pokemon.findById(id).exec();
 }
 
+/**
+ * Generates a random name for the pokemon
+ */
 async function generateName() {
   const response = await axios.get(config.rng_url);
   return response.data;
 }
 
+/**
+ * Adds a pokemon to the database
+ * @param {Pokemon} pokemon 
+ */
 async function createPokemon(pokemon) {
-  // TODO: Database call
+  Pokemon.create(pokemon)
 }
 
 module.exports = {
