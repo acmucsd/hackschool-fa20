320a938a2925355ce7062bac0226d1b09a2a7ee1
Garrett Luu <garrettluu131@gmail.com>
2020-08-04 17:10:47 -0700

    Added database models

--- server/models/pokemon.js
+++ server/models/pokemon.js
@@ -5,12 +5,56 @@ const pokemonSchema = mongoose.Schema(
     name: {
       type: String,
       required: true,
+    },
+    description: {
+      type: String,
+      require: true,
+    },
+    image: {
+      type: Buffer,
+      required: true,
+    },
+    type1: {
+      type: String,
+      required: true,
+    },
+    type2: {
+      type: String,
+      required: false,
+    },
+    userId: {
+      type: Number,
+      required: false,
+    },
+    moves: {
+      type: [Move]
     }
-    // TODO: rest of fields
   }
 );
 
+const moveSchema = mongoose.Schema(
+  {
+    type: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    power: {
+      type: Number,
+      required: true,
+    }
+  }
+)
+
 const Pokemon = mongoose.model('Pokemon', pokemonSchema);
+const Move = mongoose.model('Move', moveSchema);
 
 module.exports = Pokemon;
 
