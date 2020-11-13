a12dc613a30ad413c59c5ef3941aa19307423ab9
Garrett Luu <garrettluu131@gmail.com>
2020-11-12 17:24:36 -0800

    Added part 5 database solutions

--- server/api/index.js
+++ server/api/index.js
@@ -1,49 +1,22 @@
 const express = require('express');
+const Pokemon = require('../models/pokemon');
 
 const router = express.Router();
 
-router.get('/pokemon', (req, res) => {
-    const pokemon = [
-        {
-            name: 'Pikachu',
-            description: 'the pika pokemon',
-            type1: 'Electric',
-            type2: null,
-            image: 'google.com',
-            moves: [
-                {
-                    name: 'Scratch',
-                    type: 'Normal',
-                    power: 30,
-                }
-            ]
-        },
-        {
-            name: 'Snorlax',
-            description: 'the chad pokemon',
-            type1: 'Normal',
-            type2: null,
-            image: 'google.com',
-            moves: [
-                {
-                    name: 'Surf',
-                    type: 'Water',
-                    power: 90,
-                }
-            ]
-        }
-    ]
+router.get('/pokemon', async (req, res) => {
+    const pokemon = await Pokemon.find().exec();
+
     res.status(200).json({ pokemon });
 });
 
-router.post('/pokemon', (req, res) => {
-    const { pokemon } = req.body;
-    const { name, description, type1, image, moves } = pokemon;
+router.post('/pokemon', async (req, res) => {
+    const { name, description, type1, type2, image, moves } = req.body;
     if ((!name || !description || !type1 || !image || !moves) || moves.length > 4) {
         res.status(400).json({ error: 'Invalid input' });
     } else {
-        const punchMoves = moves.filter((move) => move.name.includes('Punch'));
-        res.status(200).json({ punchMoves });
+        const newPokemon = { name, description, type1, type2, image, moves};
+        const pokemon = await Pokemon.create(newPokemon);
+        res.status(200).json({ pokemon });
     }
 });
 
--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const config = require('./config');
 const cors = require('cors');
 const router = require('./api');
 const server = express();
+const mongoose = require('mongoose');
 
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 
 server.use('/api', router);
+
+mongoose.connect(config.databaseUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true }).then(() => {
+  console.log('Connected to MongoDB database');
+});
+
 
 server.listen(config.PORT, () => {
     console.log('Server started on port ' + config.PORT);
