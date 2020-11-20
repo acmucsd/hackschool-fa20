a1df48d2bf56d8288c0bfbb6f94a896756c6dd22
YiyueMaggieMao <myykitty711@gmail.com>
2020-11-20 09:40:12 +0800

    Part 6 solutions

--- client/src/components/PokemonCard/index.jsx
+++ client/src/components/PokemonCard/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Move from '../Move/index';
+import CanvasDraw from 'react-canvas-draw';
 import './style.css'; 
 
 const PokemonCard = (props) => {
+    const movesList = props.moves.map((move, key) => {
+        return <Move key={key} type={move.type} name={move.name} power={move.power}/>
+    })
     return (
         <div className="pokemon-card-container">
             <div className="pokemon-card-container-inner">
+                <CanvasDraw
+                    hideGrid={true}
+                    disabled={true}
+                    hideInterface={true}
+                    saveData={props.image}
+                />
                 <div className="pokemoncard-data-container">
                     <p className="pokemoncard-name">{props.name}</p>
                     <p className="pokemoncard-description">{props.description}</p>
@@ -12,6 +23,12 @@ const PokemonCard = (props) => {
                         <span className="pokemon-type-1" style={{color: "#000000"}}>{props.type1}</span>
                         <span className="pokemon-type-2" style={{color: "#000000"}}>{props.type2}</span>
                     </p>
+                    <p className="pokemoncard-bold">Moves:</p>
+                    <table className="moves-table">
+                        <tbody>
+                            {movesList}
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
